1c410598870c809963f5c67c63c57278369d0415
Tanay7614 <86945887+Tanay7614@users.noreply.github.com>
2021-10-29 17:34:03 +0530

    Add files via upload

---
+++ Bubble.java
@@ -0,0 +1,29 @@
+package sample;
+
+public class Bubble {
+        public static void main(String[] args) {
+			int[] arr= {5,99,26,45,10,1};
+			bubblesort(arr);
+		}
+
+		private static void bubblesort(int[] arr) {
+			int n=arr.length;
+		      for(int i=0;i<arr.length;i++)	{
+		    	  for(int j=0;j<n-1;j++) 
+		    	  {
+		    		  //put max element at last of each iteration
+		    		  if(arr[j]>arr[j+1])
+		    		  {
+		    			  int temp=arr[j];
+		    			  arr[j]=arr[j+1];
+		    			  arr[j+1]=temp;
+		    		  }
+		    	  }
+		    	 n=n-1;
+		      }
+		      for(int i=0;i<arr.length;i++)	{
+		    	  System.out.println(arr[i]);
+		      }
+			
+		}
+}
