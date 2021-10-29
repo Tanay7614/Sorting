6898b97f653bc6af9c24411903b6dec7bb2aae6f
Tanay7614 <86945887+Tanay7614@users.noreply.github.com>
2021-10-29 17:13:21 +0530

    Search element

---
+++ BinarySearch.java
@@ -0,0 +1,45 @@
+package sample;
+
+import java.util.Scanner;
+
+public class BinarySearch {
+         public static void main(String[] args) {
+			String words[]= {"apple","banana","cherry","guava","mango","watermelon"};
+			int result=binarySearch(words);
+			if(result==1) {
+				System.out.println("The word are available");
+			}
+			else {
+				System.out.println("The word are not available");
+			}
+			
+		}
+
+		private static int binarySearch(String[] words) {
+			System.out.println("Enter a word to be search");
+			Scanner sc=new Scanner(System.in);
+			String word=sc.nextLine();
+			int str=0;
+			int flag=0;
+			int end=words.length-1;
+			while(str<=end) {
+				
+				int mid=(str+end)/2;
+				int res=word.compareTo(words[mid]);
+				  if(res > 0) {
+					  str=mid+1;
+				  }
+				  else if(res<0) {
+					  end=mid-1;
+				  }
+				  else if(res==0){
+					 
+					 flag= 1;
+					 break;
+				 }
+				  
+			}
+			return flag;
+			
+		}
+}
